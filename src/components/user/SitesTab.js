7e544378531051cac164776dae6b03f211a2c961
MattArwenLangham <m.arwen-langham@hotmail.co.uk>
2022-09-08 10:21:29 +0100

    added all data to the site info tab

--- src/components/user/SitesTab.js
+++ src/components/user/SitesTab.js
@@ -1,5 +1,6 @@
-import { Icon, Text, ListItem } from "@rneui/themed"
+import { Icon, Text, ListItem, Image } from "@rneui/themed"
 import { useEffect, useState } from "react";
+import { Linking } from "react-native";
 import { getSitesById } from "../../utils/api";
 
 const SitesTab = ({tourData: {tourSites}}) => {
@@ -18,12 +19,17 @@ const SitesTab = ({tourData: {tourSites}}) => {
     }, [])
 
     if(isLoading) return <Text>Loading sites...</Text>
-    
     return siteInfo.map(({siteId, siteName, siteDescription, siteImage, siteAddress, contactInfo, websiteLink}) => (
         <ListItem key={siteId} bottomDivider>
+                <Image source={{uri:siteImage}} style={{height: 100, width: 100}}/>
                 <ListItem.Content>
                 <ListItem.Title>{siteName}</ListItem.Title>
-                <ListItem.Subtitle><Text>{siteDescription}</Text></ListItem.Subtitle>
+                <Text>{siteDescription}</Text>
+                <Text>{siteAddress}</Text>
+                <Text>{contactInfo}</Text>
+                <Text style={{color: 'blue'}} onPress={() => Linking.openURL(websiteLink)}>
+                    Visit Website
+                </Text>
                 </ListItem.Content>
             </ListItem>
     ))
