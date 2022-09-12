00fdf89c6e3ac99c9521a7004fceb23fc9c8f159
MattArwenLangham <m.arwen-langham@hotmail.co.uk>
2022-09-12 16:24:08 +0100

    add user feedback to errorneous tour code input

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import { ImageBackground, Text, View } from "react-native" 
+import { ImageBackground, View } from "react-native" 
 import { Button, Input } from '@rneui/themed'
 import styles from '../stylesheets/home.js'
 import React from "react";
@@ -10,19 +10,28 @@ const background = { uri: "https://www.countrywalkers.com/content/uploads/2019/0
 const Home = ({navigation: {navigate}}) => {
 
   const [tourId, setTourId] = useState()
+  const [errorMessage, setErrorMessage] = useState('')
 
   const getTourById = () => {
     
     if(!tourId || tourId.match(/[^0-9]/g, '') || tourId.length < 6){
+      setErrorMessage('Please enter a 6 digit tour code')
       input.current.shake()
     } else {
+      setErrorMessage('')
       navigate('Tour', {tourId})
     }
   }
 
-  return <ImageBackground style={styles.ImageBackground} source={background} resizeMode='cover'>
+  return <ImageBackground style={styles.ImageBackground} source={background} resizeMode='cover' blurRadius={0}>
       <View style={styles.container}>
-        <Input ref={input} placeholder='Tour ID' maxLength={6} onChangeText={(text) => setTourId(text)} keyboardType='numeric'></Input>
+        <Input ref={input} placeholder='Tour ID' maxLength={6} errorMessage={errorMessage} 
+          errorStyle={{
+            position: `relative`,
+            top: -35,
+            left: '40%'
+            }} 
+          onChangeText={(text) => setTourId(text)} keyboardType='numeric'></Input>
         <Button title="Join Tour" onPress={getTourById} />
         <Button title="My Tours" onPress={() => navigate('MyTours')} ></Button>
       </View>
