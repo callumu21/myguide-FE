f02c1cc6a701b19e3e848f41dde9912b88161ab3
MattArwenLangham <m.arwen-langham@hotmail.co.uk>
2022-09-13 10:21:41 +0100

    add loading bar to screens for longer load times

--- src/utils/Loading.js
+++ src/utils/Loading.js
@@ -1,9 +1,15 @@
-import { Text } from "@rneui/themed"
+import { Text, LinearProgress } from "@rneui/themed"
 import { View } from "react-native"
+import styles from "../stylesheets/join-tour"
 
 const Loading = ({loadMessage}) => {
     return <View>
-        <Text>{loadMessage}</Text>
+        <Text style={styles.loadMessage}>{loadMessage}</Text>
+        <LinearProgress
+      variant="indeterminate"
+      style={{ width: "90%", alignSelf: 'center', height: 8}}
+      color="rgb(247, 167, 108)"
+    />  
     </View>
 }
 
